1055b9164f3dad68f9aa5b108639c6b5abdf581d
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-03-30 16:09:55 -0700

    display the deck of cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-$("form#deck").submit(function(event) {
-  event.preventDefault();
-  var cards = ['ace of spades', '2 of clubs', '3 of clubs'];
-  cards.forEach(function(deck){
-    $('.output').append("<li>" + deck + "</li>")
-  })
-}
-)};
+  $("form#deck").submit(function(event) {
+    event.preventDefault();
+    var cards = ['ace of spades', '2 of clubs', '3 of clubs'];
+    cards.forEach(function(deck){
+     $('.output').append("<li>" + deck + "</li>");
+    });
+  });
+});
