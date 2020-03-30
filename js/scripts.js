6c7d8510c7ed25e172a8b7af540d4f54861b4891
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-03-30 16:26:50 -0700

    display suit type to user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   $("form#deck").submit(function(event) {
     event.preventDefault();
-    var cards = ['ace of spades', '2 of clubs', '3 of clubs'];
-    cards.forEach(function(deck){
+    var suits = ["of spades", "of hearts", "of diamonds", "of clubs"];
+    var number = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+    suits.forEach(function(deck){
      $('.output').append("<li>" + deck + "</li>");
     });
   });
